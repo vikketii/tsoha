ff713b6db5c7fc6afdfe9bf618250b25d65c1fc1
Vikke Tiirola <vikke.tiirola@gmail.com>
2019-10-03 15:27:59 +0300

    added song links to artist page and cleaning

--- application/__init__.py
+++ application/__init__.py
@@ -40,7 +40,6 @@ app.config['SECRET_KEY'] = urandom(32)
 from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
-
 login_manager.login_view = 'auth_login'
 login_manager.login_message = 'Please login to use this functionality.'
 
--- application/artists/models.py
+++ application/artists/models.py
@@ -4,21 +4,28 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy.sql import text
 
 song_artist = db.Table('song_artist',
-    db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True),
-    db.Column('song_id', db.Integer, db.ForeignKey('song.id'), primary_key=True),
-    db.PrimaryKeyConstraint('artist_id', 'song_id')
-)
+                       db.Column('artist_id', db.Integer, db.ForeignKey(
+                           'artist.id'), primary_key=True),
+                       db.Column('song_id', db.Integer, db.ForeignKey(
+                           'song.id'), primary_key=True),
+                       db.PrimaryKeyConstraint('artist_id', 'song_id')
+                       )
 
 album_artist = db.Table('album_artist',
-    db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True),
-    db.Column('album_id', db.Integer, db.ForeignKey('album.id'), primary_key=True),
-    db.PrimaryKeyConstraint('artist_id', 'album_id')
-)
+                        db.Column('artist_id', db.Integer, db.ForeignKey(
+                            'artist.id'), primary_key=True),
+                        db.Column('album_id', db.Integer, db.ForeignKey(
+                            'album.id'), primary_key=True),
+                        db.PrimaryKeyConstraint('artist_id', 'album_id')
+                        )
+
 
 class Artist(Base):
 
-    song_artist = db.relationship('Song', secondary=song_artist, backref=db.backref('artists', lazy='dynamic'))
-    album_artist = db.relationship('Album', secondary=album_artist, backref=db.backref('artists', lazy='dynamic'))
+    song_artist = db.relationship(
+        'Song', secondary=song_artist, backref=db.backref('artists', lazy='dynamic'))
+    album_artist = db.relationship(
+        'Album', secondary=album_artist, backref=db.backref('artists', lazy='dynamic'))
 
     def __init__(self, name):
         self.name = name
@@ -36,17 +43,23 @@ class Artist(Base):
 
     @staticmethod
     def find_artist_and_all_songs(id):
-        stmt = text("""SELECT artist.name AS artist_name, song.name AS song_name FROM Artist
-                        LEFT JOIN song_artist AS song_artist_1 ON artist.id = song_artist_1.artist_id
-                        LEFT JOIN song ON song.id = song_artist_1.song_id
+        stmt = text("""SELECT artist.id, artist.name, song.id, song.name FROM Artist
+                        LEFT JOIN song_artist ON artist.id = song_artist.artist_id
+                        LEFT JOIN song ON song.id = song_artist.song_id
                         WHERE artist.id = :id
                         """).params(id=id)
 
         res = db.engine.execute(stmt)
-        response = {"name": '', "songs": [], "count": 0}
+        response = {"songs": [], "count": 0}
         for row in res:
-            response['name'] = row[0]
-            response['songs'].append(row[1])
-            response['count'] += 1
+            response['id'] = row[0]
+            response['name'] = row[1]
+            if row[2] and row[3]:
+                response['songs'].append({'id': row[2], 'name': row[3]})
+                response['count'] += 1
+
+        print('<------')
+        print(response)
+        print('------>')
 
         return response
